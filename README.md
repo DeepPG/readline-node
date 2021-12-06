fb32999c968e196338d95aa9522599ad5f3602dd
Your Name <alialkderani@gmail.com>
2021-12-06 08:48:48 -0800

    getting Started

---
+++ README.md
@@ -0,0 +1,5 @@
+﻿# About readline node js
+
+you can say it's course abotu readline, but actually this is my secret tests and I'm using github to storage it, I think here you can find anythink you want (Not right now). and thanks for reading!
+
+
---
+++ files/1-gettingStarted.js
@@ -0,0 +1,15 @@
+﻿const readline = require("readline"); // node module
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("What is your IP ?: ", function(ip) {
+    console.log(`You has been hacked lol this is your IP :${ip}, give me 0.1$ if you wan't to be your IP being secret`); // message after answer
+    rl.close(); // closing the rl,
+});
+
+rl.on("close", function() { // close event if rl closed
+    console.log("closed");
+    process.exit(0); // exit from node js program
+});
